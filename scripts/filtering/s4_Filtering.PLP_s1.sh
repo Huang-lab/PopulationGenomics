949892c1affa9b25043766270341da5f6eea24fb
Rita Jui-Hsien Lu <38308132+RitataLU@users.noreply.github.com>
2024-10-30 14:52:26 -0400

    Update s4_Filtering.PLP_s1.sh

--- scripts/filtering/s4_Filtering.PLP_s1.sh
+++ scripts/filtering/s4_Filtering.PLP_s1.sh
@@ -16,4 +16,4 @@ ml bcftools
 
 variants='/sc/arion/projects/rg_huangk06/variants_PLP_BioMe/out/s3_ACMG32_cancer_gene.predis.plp.txt'
 vcf_in="/sc/arion/projects/rg_huangk06/variants_PLP_BioMe/data/BioMe_Sema4_WES.vcf.gz"
-vcf_out="/sc/arion/projects/rg_huangk06/variants_PLP_BioMe/out/s4
+vcf_out="/sc/arion/projects/rg_huangk06/variants_PLP_BioMe/out/s4_BioMe_Sema4_FilterExomesByPre.PLP.vcf
